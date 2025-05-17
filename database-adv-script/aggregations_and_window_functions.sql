b4e245c5acc94a1155db17dc49ae4b47f245001a
OlukaGibson <gibsonoluka7@gmail.com>
2025-05-17 18:53:35 +0300

    week 2 done

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,20 +1,18 @@
 -- 1. Total number of bookings made by each user
 SELECT
-    users.id AS user_id,
-    users.name AS user_name,
-    COUNT(bookings.id) AS total_bookings
+    user_id,
+    COUNT(*) AS total_bookings
 FROM
-    users
-LEFT JOIN bookings ON users.id = bookings.user_id
-GROUP BY users.id, users.name;
+    bookings
+GROUP BY
+    user_id;
 
 -- 2. Rank properties based on the total number of bookings
 SELECT
-    properties.id AS property_id,
-    properties.name AS property_name,
-    COUNT(bookings.id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(bookings.id) DESC) AS rank
+    property_id,
+    COUNT(*) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rank
 FROM
-    properties
-LEFT JOIN bookings ON properties.id = bookings.property_id
-GROUP BY properties.id, properties.name;
+    bookings
+GROUP BY
+    property_id;
