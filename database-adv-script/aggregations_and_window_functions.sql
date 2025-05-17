b5b82007769c4e64b3fe7c6517319bced5c7caee
OlukaGibson <gibsonoluka7@gmail.com>
2025-05-17 18:55:14 +0300

    week 2 done

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,7 +11,7 @@ GROUP BY
 SELECT
     property_id,
     COUNT(*) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rank
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
 FROM
     bookings
 GROUP BY
