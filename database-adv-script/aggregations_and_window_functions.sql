6b003c628ab999b1e08fff33cf5ea93ff88a2548
OlukaGibson <gibsonoluka7@gmail.com>
2025-05-17 18:58:40 +0300

    week 2 done

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,11 +7,21 @@ FROM
 GROUP BY
     user_id;
 
--- 2. Rank properties based on the total number of bookings
+-- 2. Rank properties based on the total number of bookings using RANK
 SELECT
     property_id,
     COUNT(*) AS total_bookings,
     RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM
+    bookings
+GROUP BY
+    property_id;
+
+-- 3. Rank properties using ROW_NUMBER
+SELECT
+    property_id,
+    COUNT(*) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_num
 FROM
     bookings
 GROUP BY
